5d88a56ad56f5553163f7297daa0e795e503d8df
Дмитрий Данилов <kertarus777@mail.ru>
2022-06-30 16:54:31 +0500

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return HttpResponse('Сначала не было ничего')
+
+
+def group_posts(request, slug):
+    return HttpResponse(f'Тут кто-то что-то написал про {slug}')
