39712884f7b767a2db5e6b1450147591742db233
Дмитрий Данилов <kertarus777@mail.ru>
2022-07-03 14:10:06 +0500

    Вариант для проверки работоспособности

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,22 @@
+from multiprocessing import context
+from re import template
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Сначала не было ничего')
+    template = 'posts/index.html'
+    title = 'Это главная страница проекта Yatube'
+    context = {
+        'title': title,
+    }
+    return render(request, template, context) 
 
 
-def group_posts(request, slug):
-    return HttpResponse(f'Тут кто-то что-то написал про {slug}')
+def group_posts(request):
+    template = 'posts/group_list.html'
+    text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'text': text,
+    }
+    return render(request, template, context)
